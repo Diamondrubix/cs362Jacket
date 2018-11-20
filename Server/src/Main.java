f69f841f19a5b195310fecf6802ed79720268e33
diamondrubix <Diamondrubix@gmail.com>
2018-11-20 10:46:16 -0600

    another

---
+++ Server/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int port = 3001;
+        //vSystem.out.println(");
+
+        Server s = new Server(port);
+        s.start();
+        while(true){
+
+        }
+
+    }
+}
